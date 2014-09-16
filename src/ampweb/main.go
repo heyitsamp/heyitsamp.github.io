e0c0690fe7fc5e2aac812c70b9f0f7ef11c81cff
Seth W. Klein <sk@sethwklein.net>
2014-09-16 16:59:47 -0400

    Add hello world

---
+++ src/ampweb/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, world!\n"))
+	})
+	panic(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
+}
