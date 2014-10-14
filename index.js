59599a3f8316a203edfa0ac03dddec44967584b7
William S. Woodruff <williamswoodruff@gmail.com>
2014-10-14 00:41:38 -0400

    Setup the feature section. The feature section borders resize on window resize. All images fade in after they load, from a universal jQuery selector adding a "load" callback to every image. I should probably change that. But maybe not, we'll see.

--- index.js
+++ index.js
@@ -1,112 +1,119 @@
 $(function()    {
-    initUI();
-});
+
+/******************************
+PUBLIC PROPERTIES
+*******************************/
+/*Eventually populate images array from XML file (a CMS)*/
+var companyNames = [
+    "atomic",
+    "branding",
+    "cocacola",
+    "disney",
+    "eb",
+    "kmd",
+    "ms",
+    "obama",
+    "pg"
+]
+var logoVersions = [
+    ["amp-00ffff.png","#00ffff"], 
+    ["amp-33ff00.png","#33ff00"], 
+    ["amp-ff33cc.png","#ff33cc"], 
+    ["amp-ff0066.png","#ff0066"], 
+    ["amp-ff9900.png","#ff9900"], 
+    ["amp-ffcc00.png","#ffcc00"]
+]
+
+
+/******************************
+PRIVATE PROPERTIES
+*******************************/
+var imgFadeInSpeed = 3000;
+var logoTextFadeInSpeed = 3000;
+
+
+/**********************
+COMMENCE THE INITIATION!!! MUAHAHAHAHAAAAA!!!!
+***********************/
+initUI();
+
 
 function initUI(){
-    
-    /*
-    setup tile image load callbacks (so they can center themselves based on their own width
-    and height
-    */
-    
-    //Eventually populate images array from XML file (a CMS)
-    var images = [
-        "atomic",
-        "branding",
-        "cocacola",
-        "disney",
-        "eb",
-        "kmd",
-        "ms",
-        "obama",
-        "pg"
-    ]
-    
-    
-    
-    
-    var j = images.length;
+    /*This is where the image loading magic happens!*/
+    var j = companyNames.length;
     for(var i=0; i<j; i++){
-        
-        /*
-        $("#" + images[i] + " img").load(onTileImgLoad($(this)));
-        REMEMBER!!! logoImg here is the div holding the <img> tags!!!
-        */
-        
-        var tileImgContainer = $("#" + images[i] + " img");
-        
-        /*
-        this is where the image loading magic happens!
-        BUG
+        var tileImgContainer = $("#" + companyNames[i] + " img");
+        /*BUG =(
         idk why, but this load stuff only works if I wrap
         the callback in that anonymous function
         */
-        tileImgContainer.attr("src", images[i] + ".png").css("opacity","0").load(function(){
-            onTileImgLoad($(this))
+        tileImgContainer.attr("src", companyNames[i] + ".png").load(function(){
+            centerImgAfterLoad($(this))
         });
         
     }
     
-    
-    var logoVersions = [
-        ["amp-00ffff.png","#00ffff"], 
-        ["amp-33ff00.png","#33ff00"], 
-        ["amp-ff33cc.png","#ff33cc"], 
-        ["amp-ff0066.png","#ff0066"], 
-        ["amp-ff9900.png","#ff9900"], 
-        ["amp-ffcc00.png","#ffcc00"]
-    ]
-    
     /*
-    Preload each logo version so we can cycle thru in a cool effect at the top
-    HAVEN'T DONE THIS YET, WANT TO CYCLE THROUGH THE COLORS QUICKLY 
+    Every image starts out invisible, until it loads, and then
+    fades in. This is fine for now. It stops jumpyness on every image
+    */
+    $("img").css("opacity", "0").load(function(){
+        imgFadeInAfterLoad($(this));
+    });
+    
+    /* SPECIAL EFFECT
+    Haven't done this yet,
+    want to cycle through the colors
+    quickly in a special effect
+    -----------------------------------
+    Need to do this for the effect----->
+    Preload each logo version so we can cycle thru
+    in a cool effect at the top 
     */
     
-    //random amp logo
+    /*randomize the amp logo*/
     var randLogo = logoVersions[Math.floor(Math.random()*logoVersions.length)];
     $("#logoImgContainer img").attr("src", randLogo[0]).load(function(){
         logoLoadCallback($(this));
     });
     
-    /*
-    Hiding the logo and the text
-    */
-    $("#logoImgContainer img").css("opacity", "0");
-    $("#logoText").css("opacity", "0");
-    $("#logoText").css("color", randLogo[1]);
+    /*Hiding the logo text*/
+    $("#logoText").css("opacity", "0").css("color", randLogo[1]);
+    
+    
+    /*Setting up the feature section*/
+    $("#featureSectionTopBorderContainer img").attr("src", "shadTop.png");
+    $("#featureSectionBottomBorderContainer img").attr("src", "shadBottom.png");
+    $( window ).resize(onWindowResize());
     
     /*
-    Set the height of the html element --- logoLockup.. Otherwise the website jumps
+    Set the height of the html element --- logoLockup.. 
+    Otherwise the website jumps
     when this image loads
-    */
-    
-    /*
+    ------------------------------------------------------
     $("#logoLockup").css("height", $("logoImgContainer img").clientHeight);
-    
-    
     Hardcoded =((((( I couldn't get the above to work and was spending too
     much time on it=((((
     */
+    
     $("#logoLockup").css("height", "115");
 }
 
+function onWindowResize(){
+    $("#featureSectionTopBorderContainer img").css("width", "100%");
+    $("#featureSectionBottomBorderContainer img").css("width", "100%");
+}
+
 function logoLoadCallback(logo){
-    //logo.css.("height", logo.height());
-    logo.animate({opacity:100}, 3000);
-    $("#logoText").animate({opacity: 100}, 3000);
+    /*logo.css.("height", logo.height());*/
+    $("#logoText").animate({opacity: 100}, logoTextFadeInSpeed);
 }
 
-function fadeInAfterLoad(jqImgTagObj){
-    jqImgTagObj.fadeIn();
+function imgFadeInAfterLoad(jqImgTagObj){
+    jqImgTagObj.animate({opacity: 100}, imgFadeInSpeed);
 }
 
-function onTileImgLoad(tileImgContainer){
-    /*
-    this also uses jQuery's fadein
-    on logo load, center the logo horizontally and vertically!
-    trace($(imgContainer).attr("src"));
-    */
-    
+function centerImgAfterLoad(tileImgContainer){
     /*horizontally center logos inside their tiles*/
     tileImgContainer.css("left", 
         tileImgContainer.parent().width()/2
@@ -118,11 +125,6 @@ function onTileImgLoad(tileImgContainer){
         tileImgContainer.parent().height()/2
         -( tileImgContainer.height()/2)
     )
-    
-    /*
-    Yeah, Yeah, I know what's with the opacity ---- remember we're Flash guys at heart though!!!!!!!
-    */
-    tileImgContainer.animate({opacity:100}, 4000);
 }
 
 
@@ -131,3 +133,6 @@ function onTileImgLoad(tileImgContainer){
 function trace(str){
     console.log(str);
 }
+    
+    
+});
