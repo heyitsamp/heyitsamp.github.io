2b6e23748abc5eb026af47b5b803f0f7d216bebc
William S. Woodruff <williamswoodruff@gmail.com>
2014-10-19 02:38:54 -0400

    Can change a single svg logo to any color with code

--- js/index.js
+++ js/index.js
@@ -12,6 +12,8 @@ $(function(){
         "#ffcc00"
     ]
     
+    var colorsUsedAlready = [];
+    
     /******************************
     PRIVATE PROPERTIES
     *******************************/
@@ -43,7 +45,9 @@ $(function(){
         Every image starts out invisible, until it loads, and then
         fades in. This is fine for now. It stops jumpyness on every image
         */
-
+        
+        //it looks very ugly if this isn't hidden in JavaScript
+        $("#logoLockup, nav").css("opacity", "0");
         $("img").css("opacity", "0").load(function(){
             imgFadeInAfterLoad($(this));
         });
@@ -66,80 +70,29 @@ $(function(){
         /*Setup the nave buttons to randomize
         the logo every time you click a nav
         button.*/
-        //$("nav li").click(randomizeLogo);
-
-
-
-        /*Set random feature (Just a random tile for now)*/
-        //LOOKING AT CLONE AND CREATING AN EMPTY
-        //jQuery object
-        var tilesContainer = $("#tilesContainer");
-        var numTiles = tilesContainer.children().length;
-        /********eq*********
-        parent.eq(index) let's you grab a child @ specific index*/
-        var randomTile = tilesContainer.eq(Math.floor(Math.random()*numTiles));
-        //$("#featureSection").append(randomTile);
-        //setFeature(randomTile);
-
-
-
-        /*
-        Set the height of the html element --- logoLockup.. 
-        Otherwise the website jumps
-        when this image loads
-        ------------------------------------------------------
-        $("#logoLockup").css("height", $("logoImgContainer img").clientHeight);
-        Hardcoded =((((( I couldn't get the above to work and was spending too
-        much time on it=((((
-        */
-
-        //$("#logoLockup").css("height", "115");
+        $("nav li").click(randomizeHeaderColor);
     }
 
     function setFeature(feature){
-        /*Feature should always be a div*/
         featureSectionJQueryObj.empty();
         featureSectionJQueryObj.append(feature);
-        /*featureSectionJQueryObj.replaceWith(feature);???
-        Actually this won't exactly work*/
     }
 
-    /*
-    This has been deprecated since the logo switched to SVG
-    
-    function logoLoadCallback(logo){
-        $("#logoText").animate({opacity: 100}, logoTextFadeInSpeed);
-    }*/
     function imgFadeInAfterLoad(jqImgTagObj){
         jqImgTagObj.animate({opacity: 100}, imgFadeInSpeed);
     }
 
     function randomizeHeaderColor() {
-        /*Random number*/
+        /*Random color*/
         var randColor = colorList[Math.floor(Math.random()*colorList.length)];
-        /*Hiding the logo text*/
         
-        trace("1");
-        
-        $("#logoText").css("color", randColor).css("opacity", "0").animate({opacity: 100}, imgFadeInSpeed);
-        
-        trace("2");
-        //$("#logo").setAttribute("fill", "white");
-        //$("#logo").css("opacity", ".5");
-        //$("#logoObject .svgPathElement").setAttribute("fill", "white");
-        //$("#logoObject path");
-        //trace("These are the children" + $("#logoObject path"));
-        var a = document.getElementById("logoObjectElement");
-        var svgDoc = a.contentDocument; //get the inner DOM of alpha.svg
-        var delta = svgDoc.getElementById("delta"); //get the inner element by id
-            delta.addEventListener("mousedown",function(){alert('hello world!')},false);    //add behaviour
-        trace("3");
-        
-        
-        
-        //$("#logoText").css("color", randLogo[1]);
+        $("#logoLockup, nav").css("opacity", "0");
+        //change to random color
+        $("#logoText").css("color", randColor);
+        $("#logoObjectElement").contents().find(".svgPathElement").attr("fill", randColor);
         $("nav a").css("color", randColor);
-        trace("4");
+        $("#logoLockup, nav").animate({opacity: 100}, 3000);
+        trace("hello?");
     }
 
 
