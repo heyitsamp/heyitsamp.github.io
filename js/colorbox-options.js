ab78cfa63eacc88cc3e5caa59488970363c60007
Seth W. Klein <sk@sethwklein.net>
2014-11-05 20:00:10 -0500

    Add link to colorbox documentation

--- js/colorbox-options.js
+++ js/colorbox-options.js
@@ -1,3 +1,4 @@
+// docs are here, toward bottom of page: http://www.jacklmoore.com/colorbox/
 var colorboxOptions = {
     // key: "value",
     // and leave the trailing comma off the last one (because javascript)
