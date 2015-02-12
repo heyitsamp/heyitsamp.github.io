e481596a9ec38ccec74bfdc76aca4671f302fc7c
Seth W. Klein <sk@sethwklein.net>
2015-02-12 18:32:31 -0500

    Add new animation system

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 /* global $, document, console, window, colorboxOptions, location, history */
 (function() {
     "use strict";
-    var scrollTime = 500,
-		revealTime = 500,
+    var revealTime = 500,
+		motionRateCap = 1000, // pixels per second
+		scrollTime = 500,
 		loadingImage = "img/spiffygif_30x30_360.gif",
 		loadingRevealTime = 50,
-		fakeNetworkDelay = 0,
         colorList = [
 			"color-cyan",
 			"color-green",
@@ -17,100 +17,342 @@
 
 	var historySupport = !!(window.history && history.pushState);
 
-	// updateFeature should be called whenever the URL might have changed,
-	// including when loading this page for the first time.
-	// It should make the document match the URL and scroll to whichever
-	// feature may be loaded.
-    var updateFeature = function(scrollLocation) {
-		var path = location.pathname,
-		    feature = path.slice(path.lastIndexOf("/")+1);
+	/*
+		Use setMotion to add new motions. Use scroll to set the scroll
+		motion.
 
-		// BUG: this reloads an existing feature. That's good if the user
-		// clicked the same feature (because then the page doesn't seem to
-		// have ignored the click), but bad if this is a newly loaded fallback.
-		$("div.feature").slideUp({
-			duration: revealTime,
-			queue: false,
-			complete: function() {
-				$(this).remove();
-			}
-		});
+		startTime:     usually set by setMotion
+		startLocation: value at motion start
+		rate:          amount value changes per ms. May be negative. Calculate
+					   using motionRate.
+		endLocation:   function returning value at motion end
+		move:          function taking a number argument giving the location
+		               to move to
+		next:          optional: function to call after animation completes
 
-        $("nav a").removeClass("current");
+		Why reimplement animation? Because both jQuery animation and
+		Velocity end animations after a set time, speeding their rate
+		as needed to do so. Here, the destination may be changing in
+		unpredictable ways, causing the rate to change similarly. In
+		contrast to others, these animations pick their rate when created
+		and continue chugging along until they reach the target value, no
+		matter how it has changed in the meantime. The scroll animation
+		has even weirder end conditions.
+	*/
+	var motions = {};
+	// scrollMotion is separate because it must run last. It also has weird
+	// termination requirements and it is the only one like that.
+	var scrollMotion;
+	// inMotion is used to prevent duplicate calls to requestAnimationFrame.
+	var inMotion = false;
 
-		if (feature.length === 0 || feature === "index.html") {
-			return;
+	// motionRate calculates the value for the rate property of a motion.
+	// Duration is in ms.
+	var motionRate = function(from, to/*, duration*/) {
+		if (from < to) {
+			return motionRateCap / 1000;
+		} else {
+			return -(motionRateCap / 1000);
+		}
+		// return (to - from) / duration;
+	};
+
+	// move is shared between all motions, including the scroll motion.
+	var move = function(motion, now) {
+		var done = false;
+		var elapsedTime = now - motion.startTime;
+		var elapsedLocation = motion.startLocation + elapsedTime * motion.rate;
+
+		// cap motion
+		var endLocation = motion.endLocation();
+		var intendedDistance = Math.abs(motion.startLocation - endLocation);
+		var currentDistance = Math.abs(motion.startLocation - elapsedLocation);
+		if (currentDistance >= intendedDistance) {
+			elapsedLocation = endLocation;
+			done = true;
 		}
 
-        $("nav a[href='"+feature+"']").addClass("current");
+		motion.move(elapsedLocation);
+		return done;
+	};
 
-		// Create a function to calculate the scroll destination because
-		// it will be moving if there is an old feature higher on the page
-		// animating closed while the scroll is happening.
-		var scrollF,
-		    square = $("#"+feature);
-		if (scrollLocation !== undefined) {
-			scrollF = function() { return scrollLocation; };
+	var areFeaturesClosing = function() {
+		for (var name in motions) {
+			if (!motions.hasOwnProperty(name)) {
+				continue;
+			}
+			if (!isFeatureName(name)) {
+				continue;
+			}
+			if (motions[name].rate >= 0) {
+				continue;
+			}
+			return true;
+		}
+		return false;
+	};
+
+	// scrollCurrent returns the current scroll value (pixel distance between
+	// document top and viewport top.
+	var scrollCurrent = function() {
+		return $(document).scrollTop();
+	};
+
+	// animate is the animation loop.
+	var animate = function() {
+		var nMotions = 0;
+		var now = $.now();
+		for (var name in motions) {
+			if (!motions.hasOwnProperty(name)) {
+				continue;
+			}
+			var motion = motions[name];
+			var done = move(motion, now);
+
+			if (done) {
+				delete motions[name];
+				if (motion.next) {
+					motion.next();
+				}
+			} else {
+				nMotions++;
+			}
+		}
+
+		if (scrollMotion) {
+			var onTarget = move(scrollMotion, now);
+			if (onTarget && !areFeaturesClosing()) {
+				scrollMotion = undefined;
+			}
+		}
+
+		if (nMotions > 0 || scrollMotion) {
+			window.requestAnimationFrame(animate);
 		} else {
-			scrollF = function() {
+			inMotion = false;
+		}
+	};
+
+	var kickAnimation = function() {
+		if (!inMotion) {
+			inMotion = true;
+			window.requestAnimationFrame(animate);
+		}
+	};
+
+	// setMotion sets the motion for name.
+	//
+	// isFeatureName(name) should be true if the motion is for a feature
+	// opening or closing, however, this feature (heh) is not currently
+	// used. It remains to save work should it prove useful in future.
+	var setMotion = function(name, motion) {
+		motion.startTime = $.now();
+		motions[name] = motion;
+		kickAnimation();
+	};
+
+	// scroll sets the scroll motion.
+	var scroll = function(to) {
+		// this animates the distance between viewport top and to(). That
+		// is subtly and critically different from animating the scroll
+		// position.
+		var distance = to() - scrollCurrent();
+		scrollMotion = {
+			startTime: $.now(),
+			startLocation: distance,
+			rate: motionRate(distance, 0, scrollTime),
+			endLocation: newZero(),
+			move: function(distance) {
+				$("html, body").scrollTop(to() - distance);
+			}
+		};
+		kickAnimation();
+	};
+
+	var isFeatureName = function(name) {
+		return name.slice(0, "feature:".length) === "feature:";
+	};
+
+	var featureName = function(slider) {
+		var cs = $(slider.children()[0]).attr("class").split(" ");
+		for (var i = 0; i < cs.length; i++) {
+			if (cs[i] !== "feature") {
+				return cs[i];
+			}
+		}
+		return "";
+	};
+
+	var newZero = function() {
+		return function() {
+			return 0;
+		};
+	};
+
+	var newRemover = function(element) {
+		return function() {
+			element.remove();
+		};
+	};
+
+	var newCorrectHeightGetter = function(slider) {
+		var feature = $(slider.children()[0]);
+		return function() {
+			return feature.height();
+		};
+	};
+
+	var newHeightSetter = function(element) {
+		return function(height) {
+			element.height(height);
+		};
+	};
+
+	var newScrollEndLocation = function(feature) {
+		var scrollEndLocation;
+		if (feature === "index") {
+			scrollEndLocation = function() {
+				return 0;
+			};
+		} else if ($("#tiles #"+feature).length > 0) {
+			// feature
+			var tile = $("#"+feature);
+			scrollEndLocation = function() {
 				// The bottom of the element is inside the visible color
 				// due to padding.
-				return square.offset().top + square.height();
+				return tile.offset().top + tile.height();
+			};
+		} else {
+			// nav
+			// this won't move
+			var location = $("header nav").offset().top;
+			scrollEndLocation = function() {
+				return location;
 			};
 		}
-		$("html, body").animate({scrollTop: scrollF()}, {
-			duration: scrollTime,
-			step: function(_, fx) {
-				fx.end = scrollF();
-			}
-		});
+		return scrollEndLocation;
+	};
 
-		var spinner = $("<div class=spinner><img src=\""+loadingImage+"\" /></div>")
-			.hide()
-			.insertAfter("#insert-"+feature)
-			.fadeIn(loadingRevealTime);
+	var currentFeatureName = function() {
+		var path = location.pathname;
+		var name = path.slice(path.lastIndexOf("/")+1);
+		if (name.length === 0 || name === "index.html") {
+			name = "index";
+		}
+		return name;
+	};
 
-		var href = "features/" + feature + ".html";
-		window.setTimeout(function() { $.ajax({
-			url: href,
-			dataType: "html",
-			error: function(xhr) {
-				// BUG(sk): test error message with 404
-				console.log("Error: "+xhr.status+" "+xhr.statusText+": "+href);
+	// groomFeatures should be called whenever something has happened that
+	// might cause a feature to be out of whack (perhaps an image loaded in).
+	// It may be called when the user has not initiated an action.
+	var groomFeatures = function(current) {
+		if (current === undefined) {
+			current = currentFeatureName();
+		}
 
-				// The browser will give a familiar error message if we do
-				// it the old fashioned way.
-				// However, after this, back is broken in IE and Chrome.
-				window.location.href = feature;
-				return;
-			},
-			success: function(data) {
-				spinner.fadeOut({
-					duration: loadingRevealTime,
-					queue: false,
-					complete: function() {
-						$(this).remove();
-					}
+		var sliders = $("div.slide");
+		for (var i = 0; i < sliders.length; i++) {
+			var slider = $(sliders[i]);
+			var name = featureName(slider);
+			var motionName = "feature:"+name;
+			var motion = motions[motionName];
+			var from;
+			if (name === current) {
+				// make sure it is open or opening
+				if (motion !== undefined && motion.rate > 0) {
+					continue;
+				}
+				var correctHeight = $(slider.children()[0]).height();
+				if (slider.height() === correctHeight) {
+					continue;
+				}
+				var to = newCorrectHeightGetter(slider);
+				from = slider.height();
+				setMotion(motionName, {
+					startLocation: from,
+					rate: motionRate(from, to(), revealTime),
+					endLocation: to,
+					move: newHeightSetter(slider)
 				});
-				$(data)
-					.hide()
-					.insertAfter(spinner)
-					.slideDown(revealTime)
-					.find("img.work").each(function() {
-						if (this.complete) {
-							return;
-						}
-						$(this).hide()
-							.on("load", function() {
-								$(this)
-									.slideDown(revealTime);
-							});
-					});
-				$("div.feature-images a").colorbox(colorboxOptions);
+			} else {
+				// make sure it is closing
+				if (motion !== undefined && motion.rate < 0) {
+					continue;
+				}
+				from = slider.height();
+				setMotion(motionName, {
+					startLocation: from,
+					rate: motionRate(from, 0, revealTime),
+					endLocation: newZero(),
+					move: newHeightSetter(slider),
+					next: newRemover(slider)
+				});
 			}
-		}); }, fakeNetworkDelay);
-    };
-    
+		}
+	};
+
+	// updateFeature should be called when the user has initiated an action
+	// that might have changed the state of the features, including when
+	// loading the page for the first time.
+    var updateFeature = function() {
+		var current = currentFeatureName();
+
+		// This appears to ignore clicks if the feature is already loaded
+		// and the page is still scrolled to it. A fix for that could be
+		// invented, but maybe later.
+
+		$("nav a").removeClass("current");
+		$("nav a[href='"+current+"']").addClass("current");
+
+		scroll(newScrollEndLocation(current));
+
+		if (current !== "index" && $("div."+current).length === 0) {
+			var spinner = $("<div class=spinner><img src=\""+loadingImage+"\" /></div>")
+				.hide()
+				.insertAfter("#insert-"+current)
+				.fadeIn(loadingRevealTime);
+
+			var href = "features/" + current + ".html";
+			$.ajax({
+				url: href,
+				dataType: "html",
+				error: function(xhr) {
+					// BUG(sk): test error message with 404
+					console.log("Error: "+xhr.status+" "+xhr.statusText+": "+href);
+
+					// The browser will give a familiar error message if we do
+					// it the old fashioned way.
+					// However, after this, back is broken in IE and Chrome.
+					window.location.href = current;
+					return;
+				},
+				success: function(data) {
+					spinner.fadeOut({
+						duration: loadingRevealTime,
+						queue: false,
+						complete: function() {
+							$(this).remove();
+						}
+					});
+					$(data)
+						.height(0)
+						.insertAfter(spinner)
+						.find("img.work").each(function() {
+							if (this.complete) {
+								return;
+							}
+							$(this).on("load", function() {
+								groomFeatures();
+							});
+						});
+					$("div.feature-images a").colorbox(colorboxOptions);
+					groomFeatures();
+				}
+			});
+		}
+		groomFeatures(current);
+	};
+
 	if (historySupport) {
 		$(window).on("popstate", function() {
 			updateFeature();
@@ -126,7 +368,7 @@
 			}
 		}
     };
-    
+
     var currentHeaderColor;
     var randomizeHeaderColor = function() {
 		var body = $("body"),
@@ -141,39 +383,25 @@
     };
 
 	$(document).ready(function() {
-		$("#tiles a").on("click", function(event) {
-			if (!historySupport) {
-				// let the browser treat it like a regular link
-				return;
-			}
-			event.preventDefault();
-
-			history.pushState(null, null, $(this).attr("href"));
-			updateFeature();
-		});
-		$("nav a, footer a, #logo").on("click", function(event) {
-			if (!historySupport) {
-				// let the browser treat it like a regular link
-				return;
-			}
-			event.preventDefault();
-
-			randomizeHeaderColor();
-
-			history.pushState(null, null, $(this).attr("href"));
-			// BUG: scrolling to page top on nav click might not be ideal
-			updateFeature(0);
-		});
-
-        $("#backToTopBtnContainer").on("click", function(){
-            $("html, body").animate({ scrollTop: 0 }, scrollTime);
-        });
-
-		$("<img />").attr("src", loadingImage);
-
         randomizeHeaderColor();
 
 		// If this is a fallback, this will scroll to its location.
 		updateFeature();
+
+        $("#backToTopBtnContainer").on("click", function(){
+			// This is slow. It could be fixed. It's not worth it right now.
+			scroll(newZero());
+        });
+
+		if (historySupport) {
+			$("<img />").attr("src", loadingImage);
+
+			$("#tiles a, nav a, footer a, #logo").on("click", function(event) {
+				event.preventDefault();
+				randomizeHeaderColor();
+				history.pushState(null, null, $(this).attr("href"));
+				updateFeature(0);
+			});
+		}
 	});
 }());
